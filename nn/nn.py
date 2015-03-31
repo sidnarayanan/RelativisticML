fixed batch learning; added multithreaded backprop

nn/nn.py
@@ -3,6 +3,7 @@
 import numpy as np
 import sys
 import cPickle as pickle
+from multiprocessing import Pool
 
 exp = np.exp
 
@@ -121,7 +122,7 @@ class Network(object):
 
   def createSummaryFile(self,fileName):
     self.summaryFile = open(fileName,"w")
-  def destroySummaryfile(self):
+  def destroySummaryFile(self):
     self.summaryFile.close()
 
   def __str__(self):
@@ -149,12 +150,27 @@ class Network(object):
         self.weights[n] -= np.outer(z,delta)*self.alpha
         # self.weights[n] = self.weights[n]/self.weights[n].sum(axis=0) # maintain normalization
 
-  def evaluateError(self,indices):
-    t=0.
-    for i in indices:
-      t+=  self.evaluate(self.dataX[i]) -  self.dataY[i] 
-    t = t/len(indices)
-    return t
+  def normalizeWeights(self):
+    for n in range(self.nLayers-2,-1,-1):
+      self.weights[n] = self.weights[n]/self.weights[n].sum(axis=0)
+
+  def hiddenBackProp(self,error):
+    # do backprop but do not apply weight differences
+    weightDelta = []
+    for n in range(self.nLayers-1,-1,-1):
+      weightDelta.append(None)
+      if n==self.nLayers-1:
+        self.outputLayer.setDeltas(error)
+      else:
+        z = self.layers[n].evaluate()
+        delta = self.layers[n+1].getDeltas()
+        self.layers[n].setDeltas(np.dot(self.weights[n],delta))
+        weightDelta[n] = np.outer(z,delta)*self.alpha
+        # self.weights[n] = self.weights[n]/self.weights[n].sum(axis=0) # maintain normalization
+    return weightDelta
+
+  def evaluateError(self,index):
+    return self.evaluate(self.dataX[i]) - self.dataY[i]
 
   def evaluatePerformance(self,indices):
     t=0.
@@ -164,9 +180,9 @@ class Network(object):
     return np.sqrt(t)    
 
   def testNetwork(self):
-    return self.evaluatePerformance(testingIndices)
+    return self.evaluatePerformance(self.testingIndices)
   def validateNetwork(self):
-    return self.evaluatePerformance(validationIndices)
+    return self.evaluatePerformance(self.validationIndices)
 
   def train(self,batchSize=1):
     if not self.dataHasBeenShuffled:
@@ -176,20 +192,29 @@ class Network(object):
     testError = self.testNetwork()
     nEpoch=0
     if self.debug:
-      self.summaryFile.write("Epoch: %i\t Error: %.3f\n"%(nEpoch,testError))
+      self.summaryFile.write("Epoch: %i\t Error: %f\n"%(nEpoch,testError))
+    j=0
+    nTrain = len(self.trainingIndices)
+    pool = Pool(6) # figure out better way of nproc later
     while True:
-      for j in len(self.data)/batchSize:
-        error = self.evaluateError(self.trainingIndices[j*batchSize:(j+1)*batchSize])
-        self.backProp(error)
+      if j > nTrain/batchSize:
+        j=0
+      batchWeightDeltas = pool.map( lambda x : self.hiddenBackProp(self.evaluateError(x)) , self.trainingIndices[j*batchSize:(j+1)*batchSize])
+      for i in range(j*batchSize,(j+1)*batchSize):
+        for k in range(self.nLayers-1):
+          self.weights[k] -= batchWeightDeltas[i][k]/float(batchSize)
+      self.normalizeWeights()
       lastError = testError
       testerror = self.testNetwork()
+      nEpoch+=1
       if self.debug:
-        self.summaryFile.write("Epoch: %i\t Error: %.3f\n"%(nEpoch,testError))
-      if testError > lastError:
+        self.summaryFile.write("Epoch: %i\t Error: %f\n"%(nEpoch,testError))
+      if testError >= lastError:
         self.alpha = self.alpha/10.
-        self.summaryFile.write("Lowering alpha to %.3f\n"%(alpha))
+        self.summaryFile.write("Lowering alpha to %.3f\n"%(self.alpha))
         if self.alpha < 0.001:
           break
+      j+=1
 
   def randomizeIndices(self,nTrain=-1, nTest=-1, nValidate=-1):
     nData = self.dataY.shape[0]

nn/vTaggingBatch.py
@@ -13,7 +13,7 @@ largeDatasets = False # use high statistics samples but have slow i/o
 negVal=0
 listOfRawVars = ["fjet1QGtagSub1","fjet1QGtagSub2","fjet1QGtag","fjet1PullAngle","fjet1Pull","fjet1MassTrimmed","fjet1MassPruned","fjet1MassSDbm1","fjet1MassSDb2","fjet1MassSDb0","fjet1QJetVol","fjet1C2b2","fjet1C2b1","fjet1C2b0p5","fjet1C2b0p2","fjet1C2b0","fjet1Tau2","fjet1Tau1"]   
 nRawVars = len(listOfRawVars)
-nHidden=5
+nHidden=1
 dims=[nRawVars+3]
 for i in xrange(nHidden):
   dims.append(nRawVars+3)
@@ -41,7 +41,7 @@ if not importClassifier:
   sigX = np.empty([sigTree.GetEntries(),nRawVars+2]) # two extra computed
   sigY = np.empty([sigTree.GetEntries()])
   bgX = np.empty([bgTree.GetEntries(),nRawVars+2]) # two extra computed
-  sigY = np.empty([bgTree.GetEntries()])
+  bgY = np.empty([bgTree.GetEntries()])
   sigcount=0
   bgcount=0
   if not largeDatasets:
@@ -118,11 +118,11 @@ if not importClassifier:
   bgX = bgX[:bgcount] # only include those that passed
   sigY = sigY[:sigcount]
   bgY = bgY[:bgcount]
-  rawDataX = np.vstack((sigX,bgX))
-  mean=rawDataX.mean(0)
-  std=rawDataX.std(0)
-  rawDataX = (rawDataX - mean)/std
-  rawDataY = np.hstack((sigY,bgY))
+  dataX = np.vstack((sigX,bgX))
+  mean=dataX.mean(0)
+  std=dataX.std(0)
+  dataX = (dataX - mean)/std
+  dataY = np.hstack((sigY,bgY))
   indices = np.arange(bgcount+sigcount)
   sigFile.Close()
   bgFile.Close()
@@ -141,8 +141,8 @@ classifier.printSummary()
 if not importWeights:
   # we should train the classifier
   nData = classifier.dataY.shape[0]
-  classifier.randomIndices(nData/3*2)
-  classifier.train()
+  classifier.randomizeIndices(nData/3*2)
+  classifier.train(1)
 
 validationIndices = classifier.validationIndices
 nBins=100
@@ -161,6 +161,7 @@ rootFile.cd()
 hSig.Write()
 hBg.Write()
 rootFile.Close()
+
 rocFile = open(argv[1]+".roc",'w')
 nSig = float(hSig.Integral())
 nBg = float(hBg.Integral())
@@ -168,3 +169,7 @@ for cut in xrange(nBins):
   sigErr = hSig.Integral(1,cut)/nSig
   bgErr = hBg.Integral(cut,nBins)/nBg
   rocFile.write("%i %f %f\n"%(cut,1-sigErr,bgErr))
+rocFile.close()
+
+classifier.destroySummaryFile()
+classifier.dumpToPickle(argv[1]+".pkl")